cb2e1f1579b39c57ab736e9affc66ace5622a348
citlalig <citlalig@Citlalis-MacBook-Air.local>
2014-09-30 22:57:48 -0500

    Initial Commit

---
+++ t1_mob_uag/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  t1_mob_uag
+//
+//  Created by Citlali Garcia on 9/30/14.
+//  Copyright (c) 2014 Personal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ t1_mob_uag/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  t1_mob_uag
+//
+//  Created by Citlali Garcia on 9/30/14.
+//  Copyright (c) 2014 Personal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
